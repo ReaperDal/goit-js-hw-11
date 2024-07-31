e04a7db775c6b9c12c75eccb7f5a63300cfa5341
ReaperDal <reaper_dal@knu.ua>
2024-07-31 17:58:22 +0300

    res
    1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+export function searchGalleryQuery(query) {
+    const URL = "https://pixabay.com/api/";
+    const API_KEY = "45091921-bafbe55c990439d7032dec8c8";
+
+    return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+        console.log(error);
+    })
+}
